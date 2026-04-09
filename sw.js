a422c1f328c76dc6438d018fdbd67eff2f741f12
saimsaim <saimkhalifa2@gmail.com>
2026-04-09 10:40:59 +0530

    feat: ads and verification live on 1924 pages

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-// 1. Monetag Config (Sabse Pehle)
+// 1. Naya Monetag Config (Updated)
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10852209
+    "domain": "5gvci.com",
+    "zoneId": 10852247
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
 
-// 2. Tera Purana Caching Logic
+// 2. Tera Purana Caching Logic (PWA)
 const CACHE_NAME = 'copyemoji-cache-v1';
 const urlsToCache = [
   '/',
@@ -15,22 +15,18 @@ const urlsToCache = [
   '/assets/data/emoji.json'
 ];
 
-// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Fetch and Cache (Offline Support)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
